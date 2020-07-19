430fe8451a96e0776d1a68ceb0566d52d0077601
weiyiming <1584879727@qq.com>
2020-07-19 23:07:29 +0800

    完成了注册页面(随便大体都是从隔壁登录页面copy来的)，用vue实现了该页面的输入检测，检测输入的密码个确认密码是否一致的功能。

---
+++ js/page5.js
@@ -0,0 +1,42 @@
+var page5 = new Vue({
+    el:".login_box",
+    data:{
+        password_1:"",
+        password_2:"",
+        error:"",
+        isError:false,
+        enterPW:"请再次输入密码：",
+    },
+    methods:{
+        errorF:function () {
+            if(this.password_1 != "" && this.password_2 !=""){
+                if(this.password_1 !== this.password_2){
+                    this.isError = true;
+                }
+            }
+            if(this.password_1 != "" && this.password_2 !=""){
+                if(this.password_1 === this.password_2){
+                    this.isError = false;
+                }
+            }
+            if (this.password_2 === ""){
+                this.isError = false;
+            }
+           this.error = (this.isError?"has-error":"");
+            this.enterPW = (this.isError?"两次输入的密码不一致":"请再次输入密码：")
+        },
+        click:function () {
+                alert("注册失败，并且不想告诉你为啥失败！");
+
+        }
+    },
+    mounted(){
+        window.chackerr = this.errorF;
+    }
+})
+
+
+window.setInterval(function () {
+    window.chackerr();
+},300)
+
